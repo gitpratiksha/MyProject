22295a19d0877d7b1613c8dbd229f1e92e7cf085
pratiksha <pratikshai.eiosys@gmail.com>
2020-07-12 14:05:53 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return FlareActor("assets/New File 10.flr", alignment:Alignment.center, fit:BoxFit.contain, animation:"Untitled");
+  }
+}
